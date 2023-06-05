ae702a224778e202b945cbc287e20257d0c96235
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-06-05 15:26:48 +0700

    Update nrj_tour.sql

--- nrj_tour.sql
+++ nrj_tour.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2023 at 08:31 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Jun 05, 2023 at 10:26 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `data_bank_admin` (
   `nama_bank` varchar(150) NOT NULL,
   `atas_nama` varchar(250) NOT NULL,
   `no_rek` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_bank_admin`
@@ -53,7 +53,7 @@ CREATE TABLE `data_bank_user` (
   `nama_bank` varchar(150) NOT NULL,
   `atas_nama` varchar(250) NOT NULL,
   `no_rek` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_bank_user`
@@ -86,15 +86,39 @@ CREATE TABLE `data_jamaah` (
   `jk` enum('Laki-laki','Perempuan') NOT NULL,
   `status_perkawinan` enum('Belum Kawin','Sudah Kawin','Cerai Hidup','Cerai Mati') NOT NULL,
   `tgl_berangkat` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_jamaah`
 --
 
 INSERT INTO `data_jamaah` (`id`, `code_order`, `foto_ktp`, `nik`, `nama`, `tempat_lahir`, `tgl_lahir`, `detail_alamat`, `prov`, `id_prov`, `kab_kota`, `id_kab_kota`, `kec`, `id_kec`, `jk`, `status_perkawinan`, `tgl_berangkat`) VALUES
-(1, 'NRJGHTH-310523001', 'assets/images/foto_ktp_jamaah/6476e5315b71c.png', '7371021303980001', 'Dody Setiawan', 'Ujung Padang', '1998-03-13', 'Jl. cendrawasih lr.4', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL),
-(2, 'NRJGHTH-010623001', 'assets/images/foto_ktp_jamaah/6477fe5b9615b.jpg', '737101030419990001', 'Ramdan Salim H', 'Makassar', '1999-04-03', 'jl cendrawasih lr 4 no 16', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL);
+(1, 'NRJGHTH-310523001', 'assets/images/foto_ktp_jamaah/6476e5315b71c.png', '7371021303980001', 'Dody Setiawan', 'Ujung Padang', '1998-03-13', 'Jl. cendrawasih lr.4', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', '2023-06-29'),
+(2, 'NRJGHTH-010623001', 'assets/images/foto_ktp_jamaah/6477fe5b9615b.jpg', '737101030419990001', 'Ramdan Salim H', 'Makassar', '1999-04-03', 'jl cendrawasih lr 4 no 16', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL),
+(5, 'NRJGHTH-050623001', 'assets/images/foto_ktp_jamaah/647d4b22f21b5.JPG', '7371021303980005', 'Asbudi Anugrah P', 'Jakarta', '1998-03-13', 'jl cendrawasih', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_kelengkapan_jamaah`
+--
+
+CREATE TABLE `data_kelengkapan_jamaah` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `code_order` varchar(20) NOT NULL,
+  `no_passport` varchar(59) NOT NULL,
+  `tgl_terbit` date NOT NULL,
+  `tgl_berlaku` date NOT NULL,
+  `alamat_terbit` text NOT NULL,
+  `is_vaksi` enum('YA','TIDAK') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data_kelengkapan_jamaah`
+--
+
+INSERT INTO `data_kelengkapan_jamaah` (`id`, `code_order`, `no_passport`, `tgl_terbit`, `tgl_berlaku`, `alamat_terbit`, `is_vaksi`) VALUES
+(3, 'NRJGHTH-310523001', 'A234567', '2023-07-27', '2023-06-30', 'MAKASSAR', 'YA');
 
 -- --------------------------------------------------------
 
@@ -111,20 +135,21 @@ CREATE TABLE `data_penjualan` (
   `is_diskon` enum('GRATIS DP & PELUNASAN','GRATIS DP','TIDAK ADA') NOT NULL,
   `uang_muka` double NOT NULL,
   `bukti_tf_uang_muka` text NOT NULL,
-  `paket_pelunasan` enum('REGULER','EKSEKUTIF','RAMADHAN','SYAWAL') DEFAULT NULL,
+  `paket_pelunasan` enum('REGULER','EKSEKUTIF','RAMADHAN','SYAWAL','GRATIS') DEFAULT NULL,
   `uang_pelunasan` double DEFAULT 0,
   `bukti_tf_pelunasan` text DEFAULT NULL,
   `status` enum('PENDING','DITOLAK','MENUNGGU PELUNASAN','MENUNGGU KONFIRMASI PELUNASAN','PELUNASAN DITOLAK','LUNAS') NOT NULL DEFAULT 'PENDING',
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_penjualan`
 --
 
 INSERT INTO `data_penjualan` (`id`, `code_order`, `perekrut`, `direkrut`, `category`, `is_diskon`, `uang_muka`, `bukti_tf_uang_muka`, `paket_pelunasan`, `uang_pelunasan`, `bukti_tf_pelunasan`, `status`, `date`) VALUES
-(1, 'NRJGHTH-310523001', 'NRJGHTH', 'NRJEUEN', 'UMROH', 'TIDAK ADA', 3500000, 'assets/images/bukti_tf_umroh/6476e53153b6c.png', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-05-31 14:12:01'),
-(2, 'NRJGHTH-010623001', 'NRJGHTH', 'NRJQNXU', 'UMROH', 'GRATIS DP', 0, 'GRATIS', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-06-01 10:11:39');
+(1, 'NRJGHTH-310523001', 'NRJGHTH', 'NRJEUEN', 'UMROH', 'TIDAK ADA', 3500000, 'assets/images/bukti_tf_umroh/6476e53153b6c.png', 'RAMADHAN', 50000000, 'assets/images/bukti_tf_umroh/647d9bf309864.JPG', 'MENUNGGU KONFIRMASI PELUNASAN', '2023-05-31 14:12:01'),
+(2, 'NRJGHTH-010623001', 'NRJGHTH', 'NRJQNXU', 'UMROH', 'GRATIS DP', 0, 'GRATIS', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-06-01 10:11:39'),
+(5, 'NRJGHTH-050623001', 'NRJGHTH', 'NRJGHTH', 'UMROH', 'GRATIS DP & PELUNASAN', 0, 'GRATIS', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-06-05 10:40:34');
 
 -- --------------------------------------------------------
 
@@ -137,7 +162,7 @@ CREATE TABLE `harga_bonus` (
   `category` enum('PENJUALAN','UPLINE') NOT NULL,
   `umroh` double NOT NULL DEFAULT 0,
   `haji` double NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `harga_bonus`
@@ -157,7 +182,7 @@ CREATE TABLE `harga_dp` (
   `id` int(11) UNSIGNED NOT NULL,
   `umroh` double NOT NULL DEFAULT 0,
   `haji` double NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `harga_dp`
@@ -178,7 +203,7 @@ CREATE TABLE `harga_pelunasan` (
   `eksekutif` double NOT NULL DEFAULT 0,
   `ramadhan` double NOT NULL DEFAULT 0,
   `syawal` double NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `harga_pelunasan`
@@ -200,7 +225,7 @@ CREATE TABLE `history_bonus_penjualan` (
   `category` enum('UMROH','HAJI') NOT NULL,
   `nominal` double NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `history_bonus_penjualan`
@@ -222,7 +247,7 @@ CREATE TABLE `pin_user` (
   `pin_uang_muka` varchar(12) NOT NULL,
   `pin_pelunasan` varchar(12) NOT NULL,
   `date_create` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pin_user`
@@ -230,7 +255,10 @@ CREATE TABLE `pin_user` (
 
 INSERT INTO `pin_user` (`id`, `code_referral`, `pin_free`, `pin_uang_muka`, `pin_pelunasan`, `date_create`) VALUES
 (1, 'NRJGHTH', 'GMPM9P5C5581', 'FSLWFZ0134M7', '96WAD2A67H0Y', '2023-05-31'),
-(2, 'NRJGHTH', 'EM1EAQEEN0RQ', 'O3XNQIJ3SQOI', 'VBYHOA52XG5V', '2023-06-01');
+(2, 'NRJGHTH', 'EM1EAQEEN0RQ', 'O3XNQIJ3SQOI', 'VBYHOA52XG5V', '2023-06-01'),
+(3, 'NRJGHTH', '47T9LULXWP67', 'VZUWO3CBJ4VE', '2X2QQ3YGFLKG', '2023-06-05'),
+(4, 'NRJQNXU', 'TYZ895N2JAI9', 'IZPMN81AIYDG', 'QWDHRB8N1L13', '2023-06-05'),
+(5, 'NRJEUEN', '4KR2LI1HEO4O', 'B5PHSTBVGY7L', 'DHEUX3OKETUX', '2023-06-05');
 
 -- --------------------------------------------------------
 
@@ -249,7 +277,7 @@ CREATE TABLE `user` (
   `upline` varchar(7) NOT NULL DEFAULT 'ADMIN',
   `status` enum('AKTIF','TIDAK AKTIF') NOT NULL DEFAULT 'TIDAK AKTIF',
   `join_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -272,14 +300,14 @@ CREATE TABLE `wallet_user` (
   `code_referral` varchar(7) NOT NULL,
   `bonus_balance` double DEFAULT 0,
   `poin_balance` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `wallet_user`
 --
 
 INSERT INTO `wallet_user` (`id`, `code_referral`, `bonus_balance`, `poin_balance`) VALUES
-(1, 'NRJGHTH', 1500000, 10);
+(1, 'NRJGHTH', 1500000, 0);
 
 -- --------------------------------------------------------
 
@@ -293,7 +321,7 @@ CREATE TABLE `withdraw` (
   `nominal` double NOT NULL,
   `status` enum('PENDING','DITOLAK','SUCCESS') NOT NULL DEFAULT 'PENDING',
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `withdraw`
@@ -324,6 +352,13 @@ ALTER TABLE `data_bank_user`
 -- Indexes for table `data_jamaah`
 --
 ALTER TABLE `data_jamaah`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `code_order` (`code_order`);
+
+--
+-- Indexes for table `data_kelengkapan_jamaah`
+--
+ALTER TABLE `data_kelengkapan_jamaah`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `code_order` (`code_order`);
 
@@ -407,13 +442,19 @@ ALTER TABLE `data_bank_user`
 -- AUTO_INCREMENT for table `data_jamaah`
 --
 ALTER TABLE `data_jamaah`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `data_kelengkapan_jamaah`
+--
+ALTER TABLE `data_kelengkapan_jamaah`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `data_penjualan`
 --
 ALTER TABLE `data_penjualan`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `harga_bonus`
@@ -443,7 +484,7 @@ ALTER TABLE `history_bonus_penjualan`
 -- AUTO_INCREMENT for table `pin_user`
 --
 ALTER TABLE `pin_user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
