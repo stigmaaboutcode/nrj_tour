2fe5fb631073910b6b0772ee86bc3b4c19cdb5f0
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-06-06 12:35:02 +0700

    meta one file

--- withdraw.php
+++ withdraw.php
@@ -13,8 +13,7 @@
     <meta charset="utf-8" />
     <title> WITHDRAW | NRJ TOUR</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesdesign" name="author" />
+    <?php include "partial/meta.php" ?>
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
