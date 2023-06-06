8daf1dc4842b27865e05591f76eca4a05ced0a91
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-06-06 13:10:52 +0700

    data dan sql

--- nrj_tour.sql
+++ nrj_tour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2023 at 10:26 AM
+-- Generation Time: Jun 06, 2023 at 08:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -94,8 +94,9 @@ CREATE TABLE `data_jamaah` (
 
 INSERT INTO `data_jamaah` (`id`, `code_order`, `foto_ktp`, `nik`, `nama`, `tempat_lahir`, `tgl_lahir`, `detail_alamat`, `prov`, `id_prov`, `kab_kota`, `id_kab_kota`, `kec`, `id_kec`, `jk`, `status_perkawinan`, `tgl_berangkat`) VALUES
 (1, 'NRJGHTH-310523001', 'assets/images/foto_ktp_jamaah/6476e5315b71c.png', '7371021303980001', 'Dody Setiawan', 'Ujung Padang', '1998-03-13', 'Jl. cendrawasih lr.4', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', '2023-06-29'),
-(2, 'NRJGHTH-010623001', 'assets/images/foto_ktp_jamaah/6477fe5b9615b.jpg', '737101030419990001', 'Ramdan Salim H', 'Makassar', '1999-04-03', 'jl cendrawasih lr 4 no 16', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL),
-(5, 'NRJGHTH-050623001', 'assets/images/foto_ktp_jamaah/647d4b22f21b5.JPG', '7371021303980005', 'Asbudi Anugrah P', 'Jakarta', '1998-03-13', 'jl cendrawasih', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', NULL);
+(2, 'NRJGHTH-010623001', 'assets/images/foto_ktp_jamaah/6477fe5b9615b.jpg', '737101030419990001', 'Ramdan Salim H', 'Makassar', '1999-04-03', 'jl cendrawasih lr 4 no 16', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', '2023-06-24'),
+(5, 'NRJGHTH-050623001', 'assets/images/foto_ktp_jamaah/647d4b22f21b5.JPG', '7371021303980005', 'Asbudi Anugrah P', 'Jakarta', '1998-03-13', 'jl cendrawasih', 'Sulawesi Selatan', 28, 'Makassar', 254, 'Mamajang', 3590, 'Laki-laki', 'Belum Kawin', '2023-06-17'),
+(6, 'NRJQNXU-060623001', 'assets/images/foto_ktp_jamaah/647ed1bb94e89.jpg', '7371020404980001', 'Ali Amran', 'Jakarta', '1989-09-14', 'tes', 'Kalimantan Utara', 16, 'Malinau', 257, 'Kayan Selatan', 3642, 'Laki-laki', 'Belum Kawin', '2023-07-08');
 
 -- --------------------------------------------------------
 
@@ -118,7 +119,10 @@ CREATE TABLE `data_kelengkapan_jamaah` (
 --
 
 INSERT INTO `data_kelengkapan_jamaah` (`id`, `code_order`, `no_passport`, `tgl_terbit`, `tgl_berlaku`, `alamat_terbit`, `is_vaksi`) VALUES
-(3, 'NRJGHTH-310523001', 'A234567', '2023-07-27', '2023-06-30', 'MAKASSAR', 'YA');
+(3, 'NRJGHTH-310523001', 'A234567', '2023-07-27', '2023-06-30', 'MAKASSAR', 'YA'),
+(4, 'NRJGHTH-010623001', 'Q212341', '2023-06-09', '2023-06-12', 'MAKASSAR', 'YA'),
+(5, 'NRJGHTH-050623001', 'QW22113', '2023-06-29', '2023-06-14', 'MAKASSAR', 'YA'),
+(6, 'NRJQNXU-060623001', 'AQ1234', '2023-04-04', '2026-06-10', 'MAKASSAR', 'YA');
 
 -- --------------------------------------------------------
 
@@ -147,9 +151,10 @@ CREATE TABLE `data_penjualan` (
 --
 
 INSERT INTO `data_penjualan` (`id`, `code_order`, `perekrut`, `direkrut`, `category`, `is_diskon`, `uang_muka`, `bukti_tf_uang_muka`, `paket_pelunasan`, `uang_pelunasan`, `bukti_tf_pelunasan`, `status`, `date`) VALUES
-(1, 'NRJGHTH-310523001', 'NRJGHTH', 'NRJEUEN', 'UMROH', 'TIDAK ADA', 3500000, 'assets/images/bukti_tf_umroh/6476e53153b6c.png', 'RAMADHAN', 50000000, 'assets/images/bukti_tf_umroh/647d9bf309864.JPG', 'MENUNGGU KONFIRMASI PELUNASAN', '2023-05-31 14:12:01'),
-(2, 'NRJGHTH-010623001', 'NRJGHTH', 'NRJQNXU', 'UMROH', 'GRATIS DP', 0, 'GRATIS', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-06-01 10:11:39'),
-(5, 'NRJGHTH-050623001', 'NRJGHTH', 'NRJGHTH', 'UMROH', 'GRATIS DP & PELUNASAN', 0, 'GRATIS', NULL, 0, NULL, 'MENUNGGU PELUNASAN', '2023-06-05 10:40:34');
+(1, 'NRJGHTH-310523001', 'NRJGHTH', 'NRJEUEN', 'UMROH', 'TIDAK ADA', 3500000, 'assets/images/bukti_tf_umroh/6476e53153b6c.png', 'RAMADHAN', 50000000, 'assets/images/bukti_tf_umroh/647ea49641b9c.jpg', 'MENUNGGU KONFIRMASI PELUNASAN', '2023-05-31 14:12:01'),
+(2, 'NRJGHTH-010623001', 'NRJGHTH', 'NRJQNXU', 'UMROH', 'GRATIS DP', 0, 'GRATIS', 'SYAWAL', 32000000, 'assets/images/bukti_tf_umroh/647e7fd349c4b.jpg', 'MENUNGGU KONFIRMASI PELUNASAN', '2023-06-01 10:11:39'),
+(5, 'NRJGHTH-050623001', 'NRJGHTH', 'NRJGHTH', 'UMROH', 'GRATIS DP & PELUNASAN', 0, 'GRATIS', 'GRATIS', 0, 'GRATIS', 'MENUNGGU KONFIRMASI PELUNASAN', '2023-06-05 10:40:34'),
+(6, 'NRJQNXU-060623001', 'NRJQNXU', 'NRJVDSK', 'UMROH', 'TIDAK ADA', 3500000, 'assets/images/bukti_tf_umroh/647ed1bb94047.jpg', 'EKSEKUTIF', 45000000, 'assets/images/bukti_tf_umroh/647ed23ecc8af.jpg', 'LUNAS', '2023-06-06 14:27:07');
 
 -- --------------------------------------------------------
 
@@ -232,7 +237,31 @@ CREATE TABLE `history_bonus_penjualan` (
 --
 
 INSERT INTO `history_bonus_penjualan` (`id`, `code_order`, `code_referral`, `category`, `nominal`, `date`) VALUES
-(1, 'NRJGHTH-310523001', 'NRJGHTH', 'UMROH', 2000000, '2023-06-01 12:46:39');
+(1, 'NRJGHTH-310523001', 'NRJGHTH', 'UMROH', 2000000, '2023-06-01 12:46:39'),
+(2, 'NRJQNXU-060623001', 'NRJQNXU', 'UMROH', 2000000, '2023-06-06 14:27:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_bonus_upline`
+--
+
+CREATE TABLE `history_bonus_upline` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `code_order` varchar(20) NOT NULL,
+  `code_referral` varchar(7) NOT NULL,
+  `code_referral_downline` varchar(7) NOT NULL,
+  `category` enum('UMROH','HAJI') NOT NULL,
+  `nominal` double NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `history_bonus_upline`
+--
+
+INSERT INTO `history_bonus_upline` (`id`, `code_order`, `code_referral`, `code_referral_downline`, `category`, `nominal`, `date`) VALUES
+(1, 'NRJQNXU-060623001', 'NRJGHTH', 'NRJQNXU', 'UMROH', 250000, '2023-06-06 14:08:16');
 
 -- --------------------------------------------------------
 
@@ -258,7 +287,10 @@ INSERT INTO `pin_user` (`id`, `code_referral`, `pin_free`, `pin_uang_muka`, `pin
 (2, 'NRJGHTH', 'EM1EAQEEN0RQ', 'O3XNQIJ3SQOI', 'VBYHOA52XG5V', '2023-06-01'),
 (3, 'NRJGHTH', '47T9LULXWP67', 'VZUWO3CBJ4VE', '2X2QQ3YGFLKG', '2023-06-05'),
 (4, 'NRJQNXU', 'TYZ895N2JAI9', 'IZPMN81AIYDG', 'QWDHRB8N1L13', '2023-06-05'),
-(5, 'NRJEUEN', '4KR2LI1HEO4O', 'B5PHSTBVGY7L', 'DHEUX3OKETUX', '2023-06-05');
+(5, 'NRJEUEN', '4KR2LI1HEO4O', 'B5PHSTBVGY7L', 'DHEUX3OKETUX', '2023-06-05'),
+(6, 'NRJGHTH', '4L2ROK3TVXFN', 'OKYMUX9477F3', 'MV1XGM158EN7', '2023-06-06'),
+(7, 'NRJQNXU', 'P97H0JNO70AD', '0ZH1OZL8LB76', 'II86HYEDW5T4', '2023-06-06'),
+(8, 'NRJEUEN', '4FBJD7M7M01H', 'JYWFN4AH5M24', 'OHUUKBPJNVPZ', '2023-06-06');
 
 -- --------------------------------------------------------
 
@@ -287,7 +319,8 @@ INSERT INTO `user` (`id`, `code_referral`, `email`, `name`, `no_telpn`, `passwor
 (1, 'ADMIN', 'admin@nrjtour.com', 'Admin', '87756789012', '$2y$10$t2I6GcF47eB2kDQqFzaSXud5TE2HJjN4wYMMDq8wwL5jJ5mwqwy.e', 'ADMIN', 'ADMIN', 'AKTIF', '2023-05-30 02:29:06'),
 (5, 'NRJEUEN', 'setaiwan@gmailc.o', 'Dody Setiawan', '87769952345', '$2y$10$if.8PM4HcW3UEBdcDUJBl.jv0DFw8g0cDKAKRnb4w.Ey1mbOG0DZ6', 'KONSULTAN', 'NRJGHTH', 'AKTIF', '2023-05-31 14:12:01'),
 (6, 'NRJQNXU', 'salimram@gmail.com', 'Ramdan Salim', '87756401234', '$2y$10$/ufx1I/2pdKhGGpIbbJVHOCG7yH4w7lSooegLZDBVfY6m7uHgfH8y', 'KONSULTAN', 'NRJGHTH', 'AKTIF', '2023-06-01 10:11:39'),
-(7, 'NRJGHTH', 'asbudi@gmail.com', 'Asbudi Anugrah P', '87760452233', '$2y$10$RGK0ZZXqTmZj8ll0Dx.m5.jvvwXFturXJEFYvtEZEgM/AenO8pI9i', 'KONSULTAN', 'ADMIN', 'AKTIF', '2023-06-03 04:17:49');
+(7, 'NRJGHTH', 'asbudi@gmail.com', 'Asbudi Anugrah P', '87760452233', '$2y$10$RGK0ZZXqTmZj8ll0Dx.m5.jvvwXFturXJEFYvtEZEgM/AenO8pI9i', 'KONSULTAN', 'ADMIN', 'AKTIF', '2023-06-03 04:17:49'),
+(8, 'NRJVDSK', 'amran@gmail.com', 'Ali Amran', '87765459980', '$2y$10$wZxwFDH0ZBXupldu8HXE/.5IX16leoqHaCwV1RGe..8R7ukKC7GTC', 'KONSULTAN', 'NRJQNXU', 'AKTIF', '2023-06-06 14:27:07');
 
 -- --------------------------------------------------------
 
@@ -307,7 +340,8 @@ CREATE TABLE `wallet_user` (
 --
 
 INSERT INTO `wallet_user` (`id`, `code_referral`, `bonus_balance`, `poin_balance`) VALUES
-(1, 'NRJGHTH', 1500000, 0);
+(1, 'NRJGHTH', 1500000, 0),
+(2, 'NRJQNXU', 2000000, 0);
 
 -- --------------------------------------------------------
 
@@ -395,6 +429,13 @@ ALTER TABLE `history_bonus_penjualan`
   ADD UNIQUE KEY `code_order` (`code_order`);
 
 --
+-- Indexes for table `history_bonus_upline`
+--
+ALTER TABLE `history_bonus_upline`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `code_order` (`code_order`);
+
+--
 -- Indexes for table `pin_user`
 --
 ALTER TABLE `pin_user`
@@ -442,19 +483,19 @@ ALTER TABLE `data_bank_user`
 -- AUTO_INCREMENT for table `data_jamaah`
 --
 ALTER TABLE `data_jamaah`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `data_kelengkapan_jamaah`
 --
 ALTER TABLE `data_kelengkapan_jamaah`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `data_penjualan`
 --
 ALTER TABLE `data_penjualan`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `harga_bonus`
@@ -478,25 +519,31 @@ ALTER TABLE `harga_pelunasan`
 -- AUTO_INCREMENT for table `history_bonus_penjualan`
 --
 ALTER TABLE `history_bonus_penjualan`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `history_bonus_upline`
+--
+ALTER TABLE `history_bonus_upline`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pin_user`
 --
 ALTER TABLE `pin_user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `wallet_user`
 --
 ALTER TABLE `wallet_user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `withdraw`
