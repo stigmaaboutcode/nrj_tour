6d3387dfe458f4ce7130d63bfed8d611f32d0cf8
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-06-08 15:27:30 +0700

    ada bug server cpanel

--- apiDb.php
+++ apiDb.php
@@ -1,8 +1,22 @@
 <?php 
 // FILE CONTROLLER DATABASE 
-foreach (glob("modal/*.php") as $database) {
-    include_once $database;
-}
+include_once "modal/ConnectionsClass.php";
+include_once "modal/dataBankClass.php";
+include_once "modal/dataBankUserClass.php";
+include_once "modal/dataJamaah.php";
+include_once "modal/dataKelengkapanJamaah.php";
+include_once "modal/dataPenjualanClass.php";
+include_once "modal/hargaBonusClass.php";
+include_once "modal/hargaDpClass.php";
+include_once "modal/hargaPelunasanClass.php";
+include_once "modal/historyBonusPenjualanClass.php";
+include_once "modal/historyBonusUplineClass.php";
+include_once "modal/pinClass.php";
+include_once "modal/rajaOngkir.php";
+include_once "modal/walletClass.php";
+include_once "modal/withdrawClass.php";
+include_once "modal/userClass.php";
+
 
 session_start();
 error_reporting(0)
